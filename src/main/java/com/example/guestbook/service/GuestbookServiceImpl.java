6e45acb6de9237be89ace8e1778c636b2d46d25d
jj9243 <jj9243@naver.com>
2022-01-12 11:31:21 +0900

    service, dto 코드 commit

---
+++ src/main/java/com/example/guestbook/service/GuestbookServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.guestbook.service;
+
+import com.example.guestbook.dto.GuestbookDTO;
+import com.example.guestbook.entity.Guestbook;
+import com.example.guestbook.repository.GuestbookRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class GuestbookServiceImpl implements GuestbookService{
+
+    private final GuestbookRepository repository;
+
+    @Override
+    public Long register(GuestbookDTO dto){
+
+        log.info("DTO--------------------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+        log.info(entity);
+
+        repository.save(entity);
+
+        return entity.getGno();
+    }
+}
